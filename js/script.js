6f4a718d8964a125181e7b72ce4ef24a793a9d72
Abdukrahman <gainaipian@hotmail.com>
2024-10-16 18:32:34 +0800

    html

--- js/script.js
+++ js/script.js
@@ -1,30 +1,4 @@
-const popup = document.getElementById('popup');
-const popupContent = document.getElementById('popup-content');
-const closeBtn = document.getElementById('close-btn');
-
-
-window.onload = function() {
-  const contentWidth = popupContent.offsetWidth;
-  const contentHeight = popupContent.offsetHeight;
-
-  popup.style.width = contentWidth + 'px'; 
-
-  popup.style.height = contentHeight + 'px'; 
-
-  const popupLeft = (window.innerWidth - popup.offsetWidth) / 2;
-  const popupTop = (window.innerHeight - popup.offsetHeight) / 2;
-
-  popup.style.left = popupLeft + 'px';
-  popup.style.top = popupTop + 'px';
-
-
-  closeBtn.addEventListener('click', () => {
-    popup.style.display = 'none';
-  }); 
-
-  document.body.addEventListener('click', function(event) {
-    if (event.target === popup) {
-      popup.style.display = 'none';
-    }
-  }); 
-}; 
+const closeBtn = document.querySelector(".close");
+closeBtn.addEventListener("click", function() {
+  document.querySelector(".announcement").style.display = "none";
+});
